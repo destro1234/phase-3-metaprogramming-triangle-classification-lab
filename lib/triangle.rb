8479cac3106e4870dbeef83a674d7e6bca1473df
Elias <eliasventura23@gmail.com>
2022-11-02 09:46:05 -0400

    finished lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+  attr_reader :sideA, :sideB, :sideC
+  # triangle code
+
+  def initialize( sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def kind
+    validate
+    if @sideA == @sideB && @sideB == @sideC
+      :equilateral
+    elsif @sideA == @sideB || @sideB == @sideC || @sideA == @sideC
+      :isosceles
+    else
+      :scalene
+     end
+
+  end
+
+  def sides_larger_than_zero
+    @sideA > 0 && @sideB > 0 && @sideC > 0 ? true : false
+  end
+
+  def inequality
+    @sideA + @sideB > @sideC && @sideB + @sideC > @sideA && @sideA + @sideC > @sideB ? true : false
+  end
+  
+  def validate
+    if sides_larger_than_zero && inequality
+      nil
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
